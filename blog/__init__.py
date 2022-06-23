dc3259b006455001a6d3be7ce772e851d5e42d48
Alex Nypels <nypels.alexandre@outlook.com>
2022-06-23 17:05:54 +0100

    Initial commit

---
+++ blog/__init__.py
@@ -0,0 +1,23 @@
+import bcrypt
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1114390d39cdc540b98a19b9c8c7ef0c '
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+
+from blog import routes
+
+
+
